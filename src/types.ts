2649915a8c3017d89be1472215dbe114ace264ec
Andrew Ross <asross@google.com>
2018-07-02 17:04:40 -0400

    add documentation and restore config

--- src/types.ts
+++ src/types.ts
@@ -16,26 +16,64 @@
  */
 
 import * as tf from '@tensorflow/tfjs';
-import {Model, Tensor, Variable} from '@tensorflow/tfjs';
+import {Model, ModelFitConfig, Tensor, Variable} from '@tensorflow/tfjs';
 import {LayerVariable} from '@tensorflow/tfjs-layers/dist/variables';
 
 export type VarList = Array<Variable|LayerVariable>;
 
+/**
+ * Basic interface that users need to implement to perform federated learning.
+ *
+ * Any model that implements `fit`, `getVars` and `setVars` can be passed into
+ * a `ClientAPI` and `ServerAPI` to do federated learning.
+ */
 export interface FederatedModel {
+  /**
+   * Trains the model to better predict the given targets.
+   *
+   * @param x `tf.Tensor` of training input data.
+   * @param y `tf.Tensor` of training target data.
+   *
+   * @return A `Promise` resolved when training is done.
+   */
   fit(x: Tensor, y: Tensor): Promise<void>;
+
+  /**
+   * Gets the model's variables.
+   *
+   * @return A list of `tf.Variable`s or LayerVariables representing the model's
+   * trainable weights.
+   */
   getVars(): VarList;
+
+  /**
+   * Sets the model's variables to given values.
+   *
+   * @param vals An array of `tf.Tensor`s representing updated model weights
+   */
   setVars(vals: Tensor[]): void;
 }
 
+/**
+ * Implementation of `FederatedModel` designed to wrap a `tf.Model`.
+ */
 export class FederatedTfModel implements FederatedModel {
   private model: Model;
+  private config: ModelFitConfig;
 
-  constructor(model: Model) {
+  /**
+   * Construct a new `FederatedModel` wrapping a `tf.Model`.
+   *
+   * @param model An instance of `tf.Model` that has already been `compile`d.
+   * @param config Optional `tf.ModelFitConfig` for training.
+   */
+  constructor(model: Model, config?: ModelFitConfig) {
     this.model = model;
+    this.config = config || {epochs: 10, batchSize: 32};
   }
 
   async fit(x: Tensor, y: Tensor): Promise<void> {
-    await this.model.fit(x, y);
+    await this.model.fit(x, y, this.config);
   }
 
   getVars(): VarList {
