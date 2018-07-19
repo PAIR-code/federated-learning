5f8adc495513b4ebe070ce7f83a0b5d49a2be16a
Aman Tiwari <tiwariaman@google.com>
2018-07-19 16:42:45 -0400

    superflous comments

--- demo/emoji_hunt/server/verify.ts
+++ demo/emoji_hunt/server/verify.ts
@@ -1,12 +1,12 @@
 import {OAuth2Client} from 'google-auth-library';
+
 const client = new OAuth2Client('834911136599-o3feieivbdf7kff50hjn1tnfmkv4noqo.apps.googleusercontent.com');
 
+// from https://developers.google.com/identity/sign-in/web/backend-auth
 export async function verify(token: string) {
   const ticket = await client.verifyIdToken({
       idToken: token,
       audience: '834911136599-o3feieivbdf7kff50hjn1tnfmkv4noqo.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
   const payload = ticket.getPayload();
   const userid = payload['sub'];
